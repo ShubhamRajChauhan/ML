434d2d37709655cd9fb0074ce49ce4f8f86b5661
Shubham <shubhamrajchauhan07@gmail.com>
2025-05-19 20:52:07 +0530

    nine

---
+++ nine.py
@@ -0,0 +1,26 @@
+#NINE
+
+from sklearn.datasets import fetch_california_housing
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error, r2_score
+from sklearn.preprocessing import StandardScaler
+
+data = fetch_california_housing()
+X,y = data.data, data.target
+
+scaler = StandardScaler()
+X_scaled = scaler.fit_transform(X)
+
+X_train, X_test, y_train, y_test = train_test_split(X_scaled, y, test_size=0.2, random_state=42)
+
+model = LinearRegression()
+model.fit(X_train, y_train)
+
+y_pred = model.predict(X_test)
+
+mse = mean_squared_error(y_test, y_pred)
+r2 = r2_score(y_test, y_pred)
+
+print(f'Mean Squared Error: {mse}')
+print(f'R2 (Coefficient of Determination): {r2}')
